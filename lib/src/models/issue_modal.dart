179e976a1ec070adcbb2797cf394fc823f009729
hivindu <h.amaradeva@gmail.com>
2021-04-19 02:02:22 +0530

    Creation of User modal

--- lib/src/models/issue_modal.dart
+++ lib/src/models/issue_modal.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-
 class Issue{
 final String id ;
 final String image ;
---
+++ lib/src/models/user_modal.dart
@@ -0,0 +1,9 @@
+class User{
+final String Id;
+final String Name ;
+final String NIC ;
+final String Password ;
+final int PostalCode ;
+
+User({this.Id,this.Name,this.NIC,this.Password,this.PostalCode});
+}
